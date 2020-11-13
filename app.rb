ad0a81f26d10c358c456e49b0fedbd38a69e299c
taylorsieling <taylorasieling@gmail.com>
2020-11-13 14:48:26 -0600

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,17 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+    get '/newteam' do
+        erb :newteam
+    end
+
+    post '/newteam' do
+        @params = params
+        erb :team
+    end
+
+    post '/team' do
+        erb :team
+    end
 
 end
